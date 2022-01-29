39be3bb27cb805c56d9d670e5b90d92f366bf8f9
Jaime Aleman <organizedfellow@gmail.com>
2022-01-29 13:26:01 -0700

    routes.js // formatting new lines for reability.

--- src/routes.js
+++ src/routes.js
@@ -4,15 +4,24 @@ import NotFound from './views/NotFound.vue'
 
 /** @type {import('vue-router').RouterOptions['routes']} */
 export const routes = [
-	{ path: '/', component: Home, meta: { title: 'Home' } },
+	{
+		path: '/',
+		component: Home,
+		meta: {
+			title: 'Home'
+		}
+	},
 	{
 		path: '/about',
-		meta: { title: 'About' },
 		component: About,
+		meta: { title: 'About' },
 		// example of route level code-splitting
 		// this generates a separate chunk (About.[hash].js) for this route
 		// which is lazy-loaded when the route is visited.
 		// component: () => import('./views/About.vue')
 	},
-	{ path: '/:path(.*)', component: NotFound },
+	{
+		path: '/:path(.*)',
+		component: NotFound
+	},
 ]
