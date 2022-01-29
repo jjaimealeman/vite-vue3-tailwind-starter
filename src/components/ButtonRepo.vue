ef153f2acb927e391643db2d43803da6b0114bf2
Jaime Aleman <organizedfellow@gmail.com>
2022-01-29 13:25:30 -0700

    ButtonRepo.vue // add rel noreferrer to button link.

--- src/components/ButtonRepo.vue
+++ src/components/ButtonRepo.vue
@@ -3,7 +3,8 @@
 		<a
 			target="_blank"
 			href="https://github.com/web2033/vite-vue3-tailwind-starter"
+			rel="noreferrer"
 			class="inline-flex items-center justify-center rounded-md border border-transparent bg-white px-5 py-3 text-base font-medium leading-6 text-indigo-600 transition duration-150 ease-in-out hover:text-indigo-500 focus:outline-none"
-			>Learn more</a>
+			>Learn s'more</a>
 	</div>
 </template>
