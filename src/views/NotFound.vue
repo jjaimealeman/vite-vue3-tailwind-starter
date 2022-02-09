b824aa68951e7046b95ce4f36e6c141a3ff31f43
Jaime Aleman <organizedfellow@gmail.com>
2022-02-08 19:46:33 -0700

    NotFound.vue // add bg image and H1 title.

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,8 +1,8 @@
 <template>
-	<header class="bg-white shadow">
-		<div class="mx-auto max-w-7xl py-6 px-4 sm:px-6 lg:px-8">
-			<h1 class="text-3xl font-bold leading-tight text-gray-900">
-				Not Found
+	<header class="bg-red-400 h-screen shadow bg-[url('https://images.unsplash.com/photo-1504537103742-67c282f65f24')] bg-cover">
+		<div class="flex flex-col items-center h-full">
+			<h1 class="text-3xl text-center font-bold my-auto leading-tight text-red-900 bg-white mx-4 p-4 opacity-50 rounded-md">
+				404 <span class="text-red-400">|</span> Page Not Found <span class="text-red-600">(uh-oh)</span>
 			</h1>
 		</div>
 	</header>
