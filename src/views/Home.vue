a35d2a068531b176a9179308f88ef3684910e25e
Jaime Aleman <organizedfellow@gmail.com>
2022-02-09 01:29:35 -0700

    Home.vue // change next page to about page.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -11,7 +11,7 @@
 					<router-link
 						to="/about"
 						class="inline-flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-5 py-3 text-base font-medium leading-6 text-white transition duration-150 ease-in-out hover:bg-indigo-500 focus:outline-none"
-						>Next Page</router-link>
+						>About Page</router-link>
 				</div>
 				<ButtonRepo />
 			</div>
