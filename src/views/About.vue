04875612545cb492225ea7109670ca4b8f0a9cd9
Jaime Aleman <organizedfellow@gmail.com>
2022-02-08 19:44:55 -0700

    About.vue // add paragraphs placeholders.

--- src/views/About.vue
+++ src/views/About.vue
@@ -16,6 +16,10 @@
 				<ButtonRepo />
 			</div>
 		</div>
+		<div class="mx-auto max-w-screen-xl px-4 py-12 sm:px-6 lg:flex lg:items-center lg:justify-between lg:py-16 lg:px-8">
+			<p class="w-1/2 p-2">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque voluptate dolore suscipit aut quis impedit facilis quia quas eum, ullam soluta doloribus tempora culpa molestiae ipsum tempore at labore animi cum minima ex. Voluptas corrupti omnis praesentium nihil voluptatem tempora eius cupiditate, libero perspiciatis dolorem officia possimus ex aliquam placeat ea ratione eveniet nostrum suscipit?</p>
+			<p class="w-1/2 p-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto cupiditate minus aliquid corrupti possimus optio omnis neque ut. Nostrum molestiae maxime quasi, neque nisi rerum illum accusamus commodi ratione nam dolorum beatae eaque enim molestias voluptatibus qui. Illum, nihil reiciendis!</p>
+		</div>
 	</div>
 </template>
 
