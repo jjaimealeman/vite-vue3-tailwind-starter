b23c807aac465bb072a1e4451f29eb0b19d6c2f0
Jaime Aleman <organizedfellow@gmail.com>
2022-02-08 19:49:24 -0700

    ++ Colors.vue // new page showing bg colors.

---
+++ src/views/Colors.vue
@@ -0,0 +1,123 @@
+<template>
+	<div class="bg-gray-50">
+		<div class="mx-auto max-w-screen-xl px-4 py-12 sm:px-6 lg:flex lg:items-center lg:justify-between lg:py-16 lg:px-8">
+			<h2 class="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 sm:text-4xl sm:leading-10">
+				About Page
+				<br />
+				<span class="text-indigo-600">Vite + Vue 3 + Tailwind CSS</span>
+<div class="bg-gray-100 text-gray-900 w-1/2 inline-block text-2xl">bg-gray-100 text-gray-900</div>
+<div class="bg-gray-200 text-gray-800 w-1/2 inline-block text-2xl">bg-gray-200 text-gray-800</div>
+<div class="bg-gray-300 text-gray-700 w-1/2 inline-block text-2xl">bg-gray-300 text-gray-700</div>
+<div class="bg-gray-400 text-gray-600 w-1/2 inline-block text-2xl">bg-gray-400 text-gray-600</div>
+<div class="bg-gray-500 text-gray-900 w-1/2 inline-block text-2xl">bg-gray-500 text-gray-900</div>
+<div class="bg-gray-600 text-gray-400 w-1/2 inline-block text-2xl">bg-gray-600 text-gray-400</div>
+<div class="bg-gray-700 text-gray-300 w-1/2 inline-block text-2xl">bg-gray-700 text-gray-300</div>
+<div class="bg-gray-800 text-gray-200 w-1/2 inline-block text-2xl">bg-gray-800 text-gray-200</div>
+<div class="bg-gray-900 text-gray-100 w-1/2 inline-block text-2xl">bg-gray-900 text-gray-100</div>
+<hr>
+<div class="bg-red-100 text-red-900 w-1/2 inline-block text-2xl">bg-red-100 text-red-900</div>
+<div class="bg-red-200 text-red-800 w-1/2 inline-block text-2xl">bg-red-200 text-red-800</div>
+<div class="bg-red-300 text-red-700 w-1/2 inline-block text-2xl">bg-red-300 text-red-700</div>
+<div class="bg-red-400 text-red-600 w-1/2 inline-block text-2xl">bg-red-400 text-red-600</div>
+<div class="bg-red-500 text-red-900 w-1/2 inline-block text-2xl">bg-red-500 text-red-900</div>
+<div class="bg-red-600 text-red-400 w-1/2 inline-block text-2xl">bg-red-600 text-red-400</div>
+<div class="bg-red-700 text-red-300 w-1/2 inline-block text-2xl">bg-red-700 text-red-300</div>
+<div class="bg-red-800 text-red-200 w-1/2 inline-block text-2xl">bg-red-800 text-red-200</div>
+<div class="bg-red-900 text-red-100 w-1/2 inline-block text-2xl">bg-red-900 text-red-100</div>
+<hr>
+<div class="bg-orange-100 text-orange-900 w-1/2 inline-block text-2xl">bg-orange-100 text-orange-900</div>
+<div class="bg-orange-200 text-orange-800 w-1/2 inline-block text-2xl">bg-orange-200 text-orange-800</div>
+<div class="bg-orange-300 text-orange-700 w-1/2 inline-block text-2xl">bg-orange-300 text-orange-700</div>
+<div class="bg-orange-400 text-orange-600 w-1/2 inline-block text-2xl">bg-orange-400 text-orange-600</div>
+<div class="bg-orange-500 text-orange-900 w-1/2 inline-block text-2xl">bg-orange-500 text-orange-900</div>
+<div class="bg-orange-600 text-orange-400 w-1/2 inline-block text-2xl">bg-orange-600 text-orange-400</div>
+<div class="bg-orange-700 text-orange-300 w-1/2 inline-block text-2xl">bg-orange-700 text-orange-300</div>
+<div class="bg-orange-800 text-orange-200 w-1/2 inline-block text-2xl">bg-orange-800 text-orange-200</div>
+<div class="bg-orange-900 text-orange-100 w-1/2 inline-block text-2xl">bg-orange-900 text-orange-100</div>
+<hr>
+<div class="bg-yellow-100 text-yellow-900 w-1/2 inline-block text-2xl">bg-yellow-100 text-yellow-900</div>
+<div class="bg-yellow-200 text-yellow-800 w-1/2 inline-block text-2xl">bg-yellow-200 text-yellow-800</div>
+<div class="bg-yellow-300 text-yellow-700 w-1/2 inline-block text-2xl">bg-yellow-300 text-yellow-700</div>
+<div class="bg-yellow-400 text-yellow-600 w-1/2 inline-block text-2xl">bg-yellow-400 text-yellow-600</div>
+<div class="bg-yellow-500 text-yellow-900 w-1/2 inline-block text-2xl">bg-yellow-500 text-yellow-900</div>
+<div class="bg-yellow-600 text-yellow-400 w-1/2 inline-block text-2xl">bg-yellow-600 text-yellow-400</div>
+<div class="bg-yellow-700 text-yellow-300 w-1/2 inline-block text-2xl">bg-yellow-700 text-yellow-300</div>
+<div class="bg-yellow-800 text-yellow-200 w-1/2 inline-block text-2xl">bg-yellow-800 text-yellow-200</div>
+<div class="bg-yellow-900 text-yellow-100 w-1/2 inline-block text-2xl">bg-yellow-900 text-yellow-100</div>
+<hr>
+<div class="bg-green-100 text-green-900 w-1/2 inline-block text-2xl">bg-green-100 text-green-900</div>
+<div class="bg-green-200 text-green-800 w-1/2 inline-block text-2xl">bg-green-200 text-green-800</div>
+<div class="bg-green-300 text-green-700 w-1/2 inline-block text-2xl">bg-green-300 text-green-700</div>
+<div class="bg-green-400 text-green-600 w-1/2 inline-block text-2xl">bg-green-400 text-green-600</div>
+<div class="bg-green-500 text-green-900 w-1/2 inline-block text-2xl">bg-green-500 text-green-900</div>
+<div class="bg-green-600 text-green-400 w-1/2 inline-block text-2xl">bg-green-600 text-green-400</div>
+<div class="bg-green-700 text-green-300 w-1/2 inline-block text-2xl">bg-green-700 text-green-300</div>
+<div class="bg-green-800 text-green-200 w-1/2 inline-block text-2xl">bg-green-800 text-green-200</div>
+<div class="bg-green-900 text-green-100 w-1/2 inline-block text-2xl">bg-green-900 text-green-100</div>
+<hr>
+<div class="bg-teal-100 text-teal-900 w-1/2 inline-block text-2xl">bg-teal-100 text-teal-900</div>
+<div class="bg-teal-200 text-teal-800 w-1/2 inline-block text-2xl">bg-teal-200 text-teal-800</div>
+<div class="bg-teal-300 text-teal-700 w-1/2 inline-block text-2xl">bg-teal-300 text-teal-700</div>
+<div class="bg-teal-400 text-teal-600 w-1/2 inline-block text-2xl">bg-teal-400 text-teal-600</div>
+<div class="bg-teal-500 text-teal-900 w-1/2 inline-block text-2xl">bg-teal-500 text-teal-900</div>
+<div class="bg-teal-600 text-teal-400 w-1/2 inline-block text-2xl">bg-teal-600 text-teal-400</div>
+<div class="bg-teal-700 text-teal-300 w-1/2 inline-block text-2xl">bg-teal-700 text-teal-300</div>
+<div class="bg-teal-800 text-teal-200 w-1/2 inline-block text-2xl">bg-teal-800 text-teal-200</div>
+<div class="bg-teal-900 text-teal-100 w-1/2 inline-block text-2xl">bg-teal-900 text-teal-100</div>
+<hr>
+<div class="bg-blue-100 text-blue-900 w-1/2 inline-block text-2xl">bg-blue-100 text-blue-900</div>
+<div class="bg-blue-200 text-blue-800 w-1/2 inline-block text-2xl">bg-blue-200 text-blue-800</div>
+<div class="bg-blue-300 text-blue-700 w-1/2 inline-block text-2xl">bg-blue-300 text-blue-700</div>
+<div class="bg-blue-400 text-blue-600 w-1/2 inline-block text-2xl">bg-blue-400 text-blue-600</div>
+<div class="bg-blue-500 text-blue-900 w-1/2 inline-block text-2xl">bg-blue-500 text-blue-900</div>
+<div class="bg-blue-600 text-blue-400 w-1/2 inline-block text-2xl">bg-blue-600 text-blue-400</div>
+<div class="bg-blue-700 text-blue-300 w-1/2 inline-block text-2xl">bg-blue-700 text-blue-300</div>
+<div class="bg-blue-800 text-blue-200 w-1/2 inline-block text-2xl">bg-blue-800 text-blue-200</div>
+<div class="bg-blue-900 text-blue-100 w-1/2 inline-block text-2xl">bg-blue-900 text-blue-100</div>
+<hr>
+<div class="bg-indigo-100 text-indigo-900 w-1/2 inline-block text-2xl">bg-indigo-100 text-indigo-900</div>
+<div class="bg-indigo-200 text-indigo-800 w-1/2 inline-block text-2xl">bg-indigo-200 text-indigo-800</div>
+<div class="bg-indigo-300 text-indigo-700 w-1/2 inline-block text-2xl">bg-indigo-300 text-indigo-700</div>
+<div class="bg-indigo-400 text-indigo-600 w-1/2 inline-block text-2xl">bg-indigo-400 text-indigo-600</div>
+<div class="bg-indigo-500 text-indigo-900 w-1/2 inline-block text-2xl">bg-indigo-500 text-indigo-900</div>
+<div class="bg-indigo-600 text-indigo-400 w-1/2 inline-block text-2xl">bg-indigo-600 text-indigo-400</div>
+<div class="bg-indigo-700 text-indigo-300 w-1/2 inline-block text-2xl">bg-indigo-700 text-indigo-300</div>
+<div class="bg-indigo-800 text-indigo-200 w-1/2 inline-block text-2xl">bg-indigo-800 text-indigo-200</div>
+<div class="bg-indigo-900 text-indigo-100 w-1/2 inline-block text-2xl">bg-indigo-900 text-indigo-100</div>
+<hr>
+<div class="bg-purple-100 text-purple-900 w-1/2 inline-block text-2xl">bg-purple-100 text-purple-900</div>
+<div class="bg-purple-200 text-purple-800 w-1/2 inline-block text-2xl">bg-purple-200 text-purple-800</div>
+<div class="bg-purple-300 text-purple-700 w-1/2 inline-block text-2xl">bg-purple-300 text-purple-700</div>
+<div class="bg-purple-400 text-purple-600 w-1/2 inline-block text-2xl">bg-purple-400 text-purple-600</div>
+<div class="bg-purple-500 text-purple-900 w-1/2 inline-block text-2xl">bg-purple-500 text-purple-900</div>
+<div class="bg-purple-600 text-purple-400 w-1/2 inline-block text-2xl">bg-purple-600 text-purple-400</div>
+<div class="bg-purple-700 text-purple-300 w-1/2 inline-block text-2xl">bg-purple-700 text-purple-300</div>
+<div class="bg-purple-800 text-purple-200 w-1/2 inline-block text-2xl">bg-purple-800 text-purple-200</div>
+<div class="bg-purple-900 text-purple-100 w-1/2 inline-block text-2xl">bg-purple-900 text-purple-100</div>
+<hr>
+<div class="bg-pink-100 text-pink-900 w-1/2 inline-block text-2xl">bg-pink-100 text-pink-900</div>
+<div class="bg-pink-200 text-pink-800 w-1/2 inline-block text-2xl">bg-pink-200 text-pink-800</div>
+<div class="bg-pink-300 text-pink-700 w-1/2 inline-block text-2xl">bg-pink-300 text-pink-700</div>
+<div class="bg-pink-400 text-pink-600 w-1/2 inline-block text-2xl">bg-pink-400 text-pink-600</div>
+<div class="bg-pink-500 text-pink-900 w-1/2 inline-block text-2xl">bg-pink-500 text-pink-900</div>
+<div class="bg-pink-600 text-pink-400 w-1/2 inline-block text-2xl">bg-pink-600 text-pink-400</div>
+<div class="bg-pink-700 text-pink-300 w-1/2 inline-block text-2xl">bg-pink-700 text-pink-300</div>
+<div class="bg-pink-800 text-pink-200 w-1/2 inline-block text-2xl">bg-pink-800 text-pink-200</div>
+<div class="bg-pink-900 text-pink-100 w-1/2 inline-block text-2xl">bg-pink-900 text-pink-100</div>
+<hr>			</h2>
+			<div class="mt-8 flex lg:mt-0 lg:flex-shrink-0">
+				<div class="inline-flex rounded-md shadow">
+					<router-link
+						to="/"
+						class="inline-flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-5 py-3 text-base font-medium leading-6 text-white transition duration-150 ease-in-out hover:bg-indigo-500 focus:outline-none"
+						>Back Home</router-link>
+				</div>
+				<ButtonRepo />
+			</div>
+		</div>
+	</div>
+</template>
+
+<script setup>
+	import ButtonRepo from "@/components/ButtonRepo.vue";
+</script>
