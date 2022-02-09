5d2d95e1e6eaa6a8e448668741d276f5df4d0045
Jaime Aleman <organizedfellow@gmail.com>
2022-02-08 19:43:32 -0700

    routes.js // add page Colors.

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,6 @@
 import Home from './views/Home.vue'
 import About from './views/About.vue'
+import Colors from './views/Colors.vue'
 import NotFound from './views/NotFound.vue'
 
 /** @type {import('vue-router').RouterOptions['routes']} */
@@ -19,6 +20,11 @@ export const routes = [
 		// this generates a separate chunk (About.[hash].js) for this route
 		// which is lazy-loaded when the route is visited.
 		// component: () => import('./views/About.vue')
+	},
+	{
+		path: '/colors',
+		component: Colors,
+		meta: { title: 'Colors' },
 	},
 	{
 		path: '/:path(.*)',
