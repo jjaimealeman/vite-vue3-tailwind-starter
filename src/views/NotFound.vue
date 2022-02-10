d9e8ecfc1969be64fa5235e353515514c4c0af96
Jaime Aleman <organizedfellow@gmail.com>
2022-02-09 19:37:40 -0700

    NotFound.vue // replace unsplash with resized local image.

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,9 +1,10 @@
 <template>
-	<header class="bg-red-400 h-screen shadow bg-[url('https://images.unsplash.com/photo-1504537103742-67c282f65f24')] bg-cover">
+	<header class="bg-red-400 h-screen shadow bg-[url('/src/views/NotFound-1280.jpg')] bg-cover">
 		<div class="flex flex-col items-center h-full">
 			<h1 class="text-3xl text-center font-bold my-auto leading-tight text-red-900 bg-white mx-4 p-4 opacity-50 rounded-md">
 				404 <span class="text-red-400">|</span> Page Not Found <span class="text-red-600">(uh-oh)</span>
 			</h1>
+			<!-- <img src="./NotFound-800.jpg" alt=""> -->
 		</div>
 	</header>
 </template>
