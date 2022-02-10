ff37c4e27855a90768695b0b67eb2faa1b3340b4
Jaime Aleman <organizedfellow@gmail.com>
2022-02-09 19:35:23 -0700

    App.vue // move router-view to the top.

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,9 @@
 		<title>Vite ++ | {{ $route.meta.title }}</title>
 	</teleport>
 	<div>
+		<main>
+			<router-view />
+		</main>
 		<header class="bg-white shadow" v-if="$route.meta.title">
 			<div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
 				<h1
@@ -12,9 +15,6 @@
 				</h1>
 			</div>
 		</header>
-		<main>
-			<router-view />
-		</main>
 	</div>
 </template>
 
