234bb9f247cea046eed2076991df6fd246a18989
Jaime Aleman <organizedfellow@gmail.com>
2022-02-11 02:37:30 -0700

    Navigation.vue // add router-link active-class.

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,37 +1,38 @@
-<!-- This example requires Tailwind CSS v2.0+ -->
 <template>
-
-    <nav class="bg-white shadow dark:bg-gray-800">
-        <div class="container flex items-center justify-center p-6 mx-auto text-gray-600 capitalize dark:text-gray-300">
-
+	<nav class="bg-white shadow bg-gray-800">
+		<div class="container flex items-center justify-center p-6 mx-auto text-gray-600 capitalize" >
 			<router-link
-				class="text-gray-800 transition-colors duration-200 transform dark:text-gray-200 border-b-2 border-blue-500 mx-1.5 sm:mx-6"
-				to="/">
+				active-class="text-white font-bold transition-colors duration-200 transform border-b-2 border-blue-500 mx-1.5 sm:mx-6"
+				class="text-gray-300 border-b-2 border-transparent hover:text-blue-400 transition-colors duration-200 transform hover:border-blue-500 mx-1.5 sm:mx-6"
+				to="/" >
 				Home
 			</router-link>
 			<router-link
-				class="border-b-2 border-transparent hover:text-gray-800 transition-colors duration-200 transform dark:hover:text-gray-200 hover:border-blue-500 mx-1.5 sm:mx-6"
-				to="/about">
+				active-class="text-white font-bold transition-colors duration-200 transform border-b-2 border-blue-500 mx-1.5 sm:mx-6"
+				class="text-gray-300 border-b-2 border-transparent hover:text-blue-400 transition-colors duration-200 transform hover:border-blue-500 mx-1.5 sm:mx-6"
+				to="/about" >
 				About
 			</router-link>
 			<router-link
-				class="border-b-2 border-transparent hover:text-gray-800 transition-colors duration-200 transform dark:hover:text-gray-200 hover:border-blue-500 mx-1.5 sm:mx-6"
-				to="/colors">
+				active-class="text-white font-bold transition-colors duration-200 transform border-b-2 border-blue-500 mx-1.5 sm:mx-6"
+				class="text-gray-300 border-b-2 border-transparent hover:text-blue-400 transition-colors duration-200 transform hover:border-blue-500 mx-1.5 sm:mx-6"
+				to="/colors" >
 				Colors
 			</router-link>
 			<router-link
-				class="border-b-2 border-transparent hover:text-gray-800 transition-colors duration-200 transform dark:hover:text-gray-200 hover:border-blue-500 mx-1.5 sm:mx-6"
-				to="/restaurants">
+				active-class="text-white font-bold transition-colors duration-200 transform border-b-2 border-blue-500 mx-1.5 sm:mx-6"
+				class="text-gray-300 border-b-2 border-transparent hover:text-blue-400 transition-colors duration-200 transform dark:hover:text-gray-200 hover:border-blue-500 mx-1.5 sm:mx-6"
+				to="/restaurants" >
 				Restaurants
 			</router-link>
 			<router-link
-				class="border-b-2 border-transparent hover:text-gray-800 transition-colors duration-200 transform dark:hover:text-gray-200 hover:border-blue-500 mx-1.5 sm:mx-6"
-				to="/NotFound">
+				active-class="text-white font-bold transition-colors duration-200 transform border-b-2 border-blue-500 mx-1.5 sm:mx-6"
+				class="text-gray-300 border-b-2 border-transparent hover:text-blue-400 transition-colors duration-200 transform hover:border-blue-500 mx-1.5 sm:mx-6"
+				to="/NotFound" >
 				404
 			</router-link>
-
-        </div>
-    </nav>
+		</div>
+	</nav>
 </template>
 
 <script>
