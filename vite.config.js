c933c7755a44057ebdd1ef98ff1e239cb0b47cd8
Jaime Aleman <jjaimealeman@gmail.com>
2022-04-05 15:42:42 -0600

    vite.config.js // add plugin: reactivityTransform.

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,18 @@ import vue from '@vitejs/plugin-vue'
 import { resolve } from 'path'
 
 export default defineConfig({
-	plugins: [vue()],
+
+	// plugins: [vue()],
+
+	// Sunday, February 20, 2022 @ 10:05:17 PM
+	// https://youtu.be/PbKWt-_oI3Q
+	// an 'opt-in' compiler time macro.
+	plugins: [
+		vue({
+			reactivityTransform: true
+		}),
+	],
+
 	resolve: {
 		alias: {
 			'@': resolve(__dirname, 'src'),
