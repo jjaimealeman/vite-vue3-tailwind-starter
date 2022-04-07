c4e4a1c8e51955f0e92834145c1cb49fba87e114
Jaime Aleman <jjaimealeman@gmail.com>
2022-04-06 21:10:46 -0600

    Home.vue // remove navigation.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,16 @@
 <template>
-	<Navigation />
-	<div class="bg-gray-50">
+	<div class="bg-gray-50 pt-12">
 		<div class="mx-auto max-w-screen-xl px-4 py-12 sm:px-6 lg:flex lg:items-center lg:justify-between lg:py-16 lg:px-8">
 			<h2 class="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 sm:text-4xl sm:leading-10">
 				Ready to dive in today Fucker?
 				<br />
-				<div class="text-indigo-600">Vite + Vue 3 + Tailwind CSS</div>
+				<div class="text-orange-600">Vite + Vue 3 + Tailwind CSS</div>
 			</h2>
 			<div class="mt-8 flex lg:mt-0 lg:flex-shrink-0">
 				<div class="inline-flex rounded-md shadow">
 					<router-link
 						to="/about"
-						class="inline-flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-5 py-3 text-base font-medium leading-6 text-white transition duration-150 ease-in-out hover:bg-indigo-500 focus:outline-none"
+						class="inline-flex items-center justify-center rounded-md border border-transparent bg-orange-600 px-5 py-3 text-base font-medium leading-6 text-white transition duration-150 ease-in-out hover:bg-orange-500 focus:outline-none"
 						>About Page</router-link>
 				</div>
 				<ButtonRepo />
@@ -19,13 +18,13 @@
 		</div>
 		<div class="mx-auto max-w-screen-xl px-4 py-12 sm:px-6 lg:flex lg:items-center lg:justify-between lg:py-16 lg:px-8">
 			<h2 class="text-2xl font-bold leading-8 tracking-normal">Check out the
-				<router-link to="/colors" class="text-indigo-600">Colors Page</router-link>.
+				<router-link to="/colors" class="text-orange-600">Colors Page</router-link>.
 			</h2>
 		</div>
 	</div>
 </template>
 
+
 <script setup>
 	import ButtonRepo from "@/components/ButtonRepo.vue";
-	import Navigation from "@/components/Navigation.vue";
 </script>
