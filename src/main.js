fbc44c9cb9a2cd53d7b48fd2eefaac3dba92fd86
Jaime Aleman <jjaimealeman@gmail.com>
2022-04-06 22:07:43 -0600

    main.js // add vueuse/head.

--- src/main.js
+++ src/main.js
@@ -3,13 +3,16 @@ import './tailwind.css'
 import App from './App.vue'
 import { routes } from './routes.js'
 import { createRouter, createWebHistory } from 'vue-router'
+import { createHead } from '@vueuse/head'
 
 const app = createApp(App)
+const head = createHead()
 
 const router = createRouter({
 	history: createWebHistory(),
 	routes,
 })
 
+app.use(head)
 app.use(router)
 app.mount('#app')
