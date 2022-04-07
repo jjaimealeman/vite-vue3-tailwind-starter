a0ee30cb801283f7a0e82c2717f4c8cbb45d0bd6
Jaime Aleman <jjaimealeman@gmail.com>
2022-04-06 21:17:12 -0600

    API.vue // remove ButtonRepo link.

--- src/views/API.vue
+++ src/views/API.vue
@@ -6,15 +6,6 @@
 				<br />
 				<div class="text-purple-600">Vite + Vue 3 + Tailwind CSS</div>
 			</h2>
-			<div class="mt-8 flex lg:mt-0 lg:flex-shrink-0">
-				<div class="inline-flex rounded-md shadow">
-					<router-link
-						to="/about"
-						class="inline-flex items-center justify-center rounded-md border border-transparent bg-purple-600 px-5 py-3 text-base font-medium leading-6 text-white transition duration-150 ease-in-out hover:bg-purple-500 focus:outline-none"
-						>About Page</router-link>
-				</div>
-				<ButtonRepo />
-			</div>
 		</div>
 		<div class="mx-auto max-w-screen-xl px-4 py-12 sm:px-6 lg:items-center lg:justify-between lg:py-16 lg:px-8">
 			<h2 class="text-3xl font-bold leading-8 tracking-normal mb-8">Some of the Best Restaurants in Town!</h2>
