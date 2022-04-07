11a4c04d0aa8ab1796a31544ccd66f0c349883ea
Jaime Aleman <jjaimealeman@gmail.com>
2022-04-06 21:04:15 -0600

    App.vue // remove counter.

--- src/App.vue
+++ src/App.vue
@@ -2,28 +2,14 @@
 	<teleport to="head">
 		<title>{{ $route.meta.title }}</title>
 	</teleport>
-	<div>
-		<main>
-			<router-view />
-		</main>
-		<header class="bg-white shadow" v-if="$route.meta.title">
-			<div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-				<h1
-					@click="counter = 0"
-					class="text-3xl font-bold leading-tight text-gray-900">
-					{{ $route.meta.title }} / {{ counter }}
-				</h1>
-			</div>
-		</header>
-	</div>
+	<main>
+		<Navigation />
+		<router-view />
+	</main>
+	<Footer />
 </template>
 
 <script setup>
-	import { ref } from "vue";
-
-	let counter = ref(0);
-
-	setInterval(() => {
-		counter.value++;
-	}, 1000);
+	import Navigation from './components/Navigation.vue';
+	import Footer from './components/Footer.vue'
 </script>
