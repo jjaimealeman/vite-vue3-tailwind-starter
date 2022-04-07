7ac1cdc32287f6588e0f29ed8ff9daab29829dd0
Jaime Aleman <jjaimealeman@gmail.com>
2022-04-06 21:08:09 -0600

    About.vue // edit text, remove navigation.

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,17 +1,16 @@
 <template>
-	<Navigation />
-	<div class="bg-gray-50">
+	<div class="bg-gray-50 pt-12">
 		<div class="mx-auto max-w-screen-xl px-4 py-12 sm:px-6 lg:flex lg:items-center lg:justify-between lg:py-16 lg:px-8">
 			<h2 class="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 sm:text-4xl sm:leading-10">
-				About Page
+				Learn a little bit About Me!?
 				<br />
-				<span class="text-indigo-600">Vite + Vue 3 + Tailwind CSS</span>
+				<span class="text-teal-600">Vite + Vue 3 + Tailwind CSS</span>
 			</h2>
 			<div class="mt-8 flex lg:mt-0 lg:flex-shrink-0">
 				<div class="inline-flex rounded-md shadow">
 					<router-link
 						to="/"
-						class="inline-flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-5 py-3 text-base font-medium leading-6 text-white transition duration-150 ease-in-out hover:bg-indigo-500 focus:outline-none"
+						class="inline-flex items-center justify-center rounded-md border border-transparent bg-teal-600 px-5 py-3 text-base font-medium leading-6 text-white transition duration-150 ease-in-out hover:bg-teal-500 focus:outline-none"
 						>Back Home</router-link>
 				</div>
 				<ButtonRepo />
@@ -24,7 +23,7 @@
 	</div>
 </template>
 
+
 <script setup>
 	import ButtonRepo from "@/components/ButtonRepo.vue";
-	import Navigation from "@/components/Navigation.vue";
 </script>
