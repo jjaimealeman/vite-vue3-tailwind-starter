0a58b5f68b9cd67cb8656a38cf68d2b1b0072964
Jaime Aleman <jjaimealeman@gmail.com>
2022-04-06 22:08:19 -0600

    routes.js // remove meta.title becuase i am using vueuse/head.

--- src/routes.js
+++ src/routes.js
@@ -9,41 +9,26 @@ export const routes = [
 	{
 		component: Home,
 		name: 'Home',
-		meta: {
-			title: 'Home Page'
-		},
 		path: '/'
 	},
 	{
 		component: About,
 		name: 'About',
-		meta: {
-			title: 'About Page'
-		},
 		path: '/about'
 	},
 	{
 		component: Colors,
 		name: 'Colors',
-		meta: {
-			title: 'Colors Test'
-		},
 		path: '/colors'
 	},
 	{
 		component: API,
 		name: 'API',
-		meta: {
-			title: 'API Listing'
-		},
 		path: '/api'
 	},
 	{
 		component: NotFound,
 		name: 'NotFound',
-		meta: {
-			title: 'Page Not Found'
-		},
 		path: '/:path(.*)'
 	},
 ]
