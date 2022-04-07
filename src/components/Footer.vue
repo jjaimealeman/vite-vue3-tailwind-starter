0be7a3ffb785a9443f1409cf495750f7c8074d2f
Jaime Aleman <jjaimealeman@gmail.com>
2022-04-06 21:06:13 -0600

    Footer.vue // add year script.

---
+++ src/components/Footer.vue
@@ -0,0 +1,23 @@
+<template>
+	<footer>
+		<p>Copyright &copy; {{ year }} </p>
+	</footer>
+</template>
+
+
+<script>
+	import { computed } from 'vue'
+	export default {
+		setup () {
+			const year = computed(() => new Date().getFullYear())
+			return { year }
+		}
+	}
+</script>
+
+
+<style>
+	footer {
+		@apply bg-gray-800 text-white w-full border-2 border-t-blue-400 p-2 text-center;
+	}
+</style>
