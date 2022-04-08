2f40b4b23855887d1246730f30fa6d240a1d5c01
Jaime Aleman <jjaimealeman@gmail.com>
2022-04-07 22:55:15 -0600

    API.vue // API working, Hoppscotch help, connect to 915websites backend 👍.

--- src/views/API.vue
+++ src/views/API.vue
@@ -2,28 +2,26 @@
 	<div class="bg-gray-50 pt-12">
 		<div class="mx-auto max-w-screen-xl px-4 py-12 sm:px-6 lg:flex lg:items-center lg:justify-between lg:py-16 lg:px-8">
 			<h2 class="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 sm:text-4xl sm:leading-10">
-				Some of my favorite places to eat.
+				Learning some API <em class="text-gray-600">stuffs</em>.
 				<br />
 				<div class="text-purple-600">Vite + Vue 3 + Tailwind CSS</div>
 			</h2>
 		</div>
 		<div class="mx-auto max-w-screen-xl px-4 py-12 sm:px-6 lg:items-center lg:justify-between lg:py-16 lg:px-8">
-			<h2 class="text-3xl font-bold leading-8 tracking-normal mb-8">Some of the Best Restaurants in Town!</h2>
-			<p>Following this tutorial <a class="text-blue-500 underline" href="https://www.digitalocean.com/community/tutorials/how-to-use-vue-js-environment-modes-with-a-node-js-mock-data-layer" target="_blank">https://www.digitalocean.com/community/tutorials/how-to-use-vue-js-environment-modes-with-a-node-js-mock-data-layer</a>, and learning how to use mock data.</p>
-			<section id="restaurant">
+			<h2 class="text-3xl font-bold leading-8 tracking-normal mb-8">My First BLOG.</h2>
+			<p class="p-2 mb-4 bg-sky-200 border-l-4 border-l-sky-400">Pulling data from my back-end server at <a class="text-blue-700 underline" href="https://915websites.com" target="_blank" title="Affordable, fast websites for the local El Paso TX Business Owner.">915websites.com</a></p>
+			<section id="blog">
 				<div v-if="error">
 					{{ error }}
 				</div>
 				<div v-else>
-					<p v-for="restaurant in restaurants" :key="restaurant.id">
-						<h2>{{ restaurant.attributes.name }}</h2>
+					<div class="mb-8" v-for="blog in blogs" :key="blog.id">
+						<h2 class="p-2 border-l-4 border-l-slate-400 border-t-2 border-t-slate-400">{{ blog.title }}</h2>
 
-						<!-- BOTH THESE FORMATS WORK!!! -->
-						<!-- <a :href="`${restaurant.attributes.url}`" target="_blank">{{ restaurant.attributes.url }}</a> -->
-						<a :href="restaurant.attributes.url" target="_blank">{{ restaurant.attributes.url }}</a>
+						<p class="italic pl-2">{{ blog.description }}</p>
 
-						<span>{{ restaurant.attributes.description }}</span>
-					</p>
+						<p class="pl-2">{{ blog.content }}</p>
+					</div>
 				</div>
 			</section>
 
@@ -33,9 +31,9 @@
 
 
 <script>
-import { defineComponent, computed, reactive } from 'vue'
-import { useHead } from '@vueuse/head'
+import { useHead } from '@vueuse/head';
 import axios from 'axios';
+import { computed, defineComponent, reactive } from 'vue';
 export default defineComponent({
 	setup() {
 		const siteData = reactive({
@@ -56,14 +54,14 @@ export default defineComponent({
 	name: 'App',
 		data () {
 			return {
-				restaurants: [],
+				blogs: [],
 				error: null
 			}
 		},
 		async mounted () {
 			try {
-				const response = await axios.get('http://localhost:1337/api/restaurants')
-				this.restaurants = response.data.data
+				const response = await axios.get('https://915websites.com/items/blog')
+				this.blogs = response.data.data
 			} catch (error) {
 				this.error = error;
 			}
@@ -73,16 +71,16 @@ export default defineComponent({
 
 
 <style scoped>
-	#restaurant p {
+	#blog p {
 		@apply py-4
 	}
-	#restaurant h2 {
+	#blog h2 {
 		@apply text-2xl font-bold leading-8 tracking-normal mb-2
 	}
-	#restaurant a {
+	#blog a {
 		@apply block underline p-0 mb-2 hover:bg-purple-200 hover:text-purple-600 hover:pl-1 transition-all
 	}
-	#restaurant span {
+	#blog span {
 		@apply prose;
 	}
 </style>
